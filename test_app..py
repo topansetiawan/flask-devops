4629b11cc35dd9774e1daa48f46d09114862f1bc
Topan Setiawan <topan.sosial@gmail.com>
2025-09-11 17:35:57 +0700

    Add Flask app with test

---
+++ test_app..py
@@ -0,0 +1,7 @@
+# test_app.py
+from app import app
+
+def test_home():
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+    assert response.data == b"Hello, DevSecOps!"
